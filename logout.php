8392ffd9ec4578f5f33b666da5103f732a0cdbb8
Gunasekhar Vemula <86915307+GUNASEKHARVEMULA@users.noreply.github.com>
2024-03-27 03:49:17 +0530

    added login page

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+// Start session to access session variables
+session_start();
+
+// Unset all session variables
+session_unset();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to login page after logout
+header("Location: index.html");
+exit
